0ff27fbb752d85fbf5550b6b18f106d37e7b6811
Sarah Soutoul <sarahsoutoul@Sarahs-MacBook-Air-2.local>
2025-04-08 16:48:25 -0600

    added CORS

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Main server logic
 // app -> routes -> controllers -> model 
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const todosRouter = require("./routes/todos");
 
+// localhost:3000/todos
+app.use(cors())
+// parsing the body for the POST and PATCH requests
 app.use(express.json())
 // middlewares
+// Setting up our express routes 
 app.use("/todos", todosRouter);
 
 
